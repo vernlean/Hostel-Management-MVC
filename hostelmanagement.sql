3fff5a191af527da634a38e1bd1eb28cebd29243
Mohamad Amirul <92899495+vernlean@users.noreply.github.com>
2022-01-29 14:05:07 +0800

    Add files via upload

--- hostelmanagement.sql
+++ hostelmanagement.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 29, 2022 at 06:35 AM
+-- Generation Time: Jan 29, 2022 at 07:04 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.0.13
 
@@ -56,6 +56,37 @@ INSERT INTO `college` (`coll_no`, `coll_name`, `coll_floor`, `coll_room`, `coll_
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `college_application`
+--
+
+CREATE TABLE `college_application` (
+  `collap_no` int(11) NOT NULL,
+  `collap_date` date NOT NULL,
+  `collap_session` varchar(255) NOT NULL,
+  `collap_status` varchar(255) NOT NULL,
+  `stu_no` int(10) NOT NULL,
+  `stf_no` int(10) NOT NULL,
+  `coll_no` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `complaint`
+--
+
+CREATE TABLE `complaint` (
+  `comp_no` int(11) NOT NULL,
+  `comp_type` varchar(255) NOT NULL,
+  `comp_date` date NOT NULL,
+  `comp_description` varchar(255) NOT NULL,
+  `comp_status` varchar(255) NOT NULL,
+  `stu_no` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `staff`
 --
 
@@ -75,6 +106,26 @@ INSERT INTO `staff` (`stf_no`, `stf_name`, `stf_pass`, `stf_position`, `stf_birt
 (101, 'Null', 'Null', 'Null', '0000-00-00'),
 (11223344, 'Abdul Aziz Bin Ali', '1234', 'Superadmin', '1982-04-08');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
+  `stu_no` int(10) NOT NULL,
+  `stu_name` varchar(255) NOT NULL,
+  `stu_ic` varchar(12) NOT NULL,
+  `stu_pass` varchar(255) NOT NULL,
+  `stu_program` varchar(255) NOT NULL,
+  `stu_state` varchar(255) NOT NULL,
+  `stu_part` int(11) NOT NULL,
+  `stu_gender` varchar(255) NOT NULL,
+  `stu_email` varchar(255) DEFAULT NULL,
+  `stu_phoneno` varchar(255) DEFAULT NULL,
+  `stu_blood` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 --
 -- Indexes for dumped tables
 --
@@ -86,10 +137,66 @@ ALTER TABLE `college`
   ADD PRIMARY KEY (`coll_no`) USING BTREE;
 
 --
+-- Indexes for table `college_application`
+--
+ALTER TABLE `college_application`
+  ADD PRIMARY KEY (`collap_no`),
+  ADD KEY `stu_no` (`stu_no`),
+  ADD KEY `stf_no` (`stf_no`),
+  ADD KEY `coll_no` (`coll_no`);
+
+--
+-- Indexes for table `complaint`
+--
+ALTER TABLE `complaint`
+  ADD PRIMARY KEY (`comp_no`),
+  ADD KEY `matrixno` (`stu_no`);
+
+--
 -- Indexes for table `staff`
 --
 ALTER TABLE `staff`
   ADD PRIMARY KEY (`stf_no`);
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`stu_no`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `college_application`
+--
+ALTER TABLE `college_application`
+  MODIFY `collap_no` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `complaint`
+--
+ALTER TABLE `complaint`
+  MODIFY `comp_no` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `college_application`
+--
+ALTER TABLE `college_application`
+  ADD CONSTRAINT `college_application_ibfk_2` FOREIGN KEY (`stu_no`) REFERENCES `student` (`stu_no`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `college_application_ibfk_3` FOREIGN KEY (`coll_no`) REFERENCES `college` (`coll_no`),
+  ADD CONSTRAINT `college_application_ibfk_4` FOREIGN KEY (`stf_no`) REFERENCES `staff` (`stf_no`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `complaint`
+--
+ALTER TABLE `complaint`
+  ADD CONSTRAINT `complaint_ibfk_1` FOREIGN KEY (`stu_no`) REFERENCES `student` (`stu_no`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
